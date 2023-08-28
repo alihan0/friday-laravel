2fdfbe8071b48338b50cd5c966659ec19b07cfc2
Alihan Öztürk <alihanozturk364@gmail.com>
2023-08-28 19:58:06 +0300

    project form id and names crated

--- resources/views/project/new.blade.php
+++ resources/views/project/new.blade.php
@@ -19,20 +19,20 @@
         <div class="col-6">
             <div class="card">
                 <div class="card-body">
-                    <form>
+                    <form id="projectForm">
                         <div class="mb-3">
                           <label for="title" class="form-label">Proje Başlığı</label>
-                          <input type="text" class="form-control" id="title" placeholder="Projenin görünen adını girin.">
+                          <input type="text" class="form-control" id="title" name="title" placeholder="Projenin görünen adını girin.">
                         </div>
                         <div class="mb-3">
                           <label for="detail" class="form-label">Açıklama</label>
-                          <textarea class="form-control" id="detail" rows="3" placeholder="Projenin detaylarından bahsedin. (Zorunlu Değil)"></textarea>
+                          <textarea class="form-control" id="detail" name="detail" rows="3" placeholder="Projenin detaylarından bahsedin. (Zorunlu Değil)"></textarea>
                         </div>
                         <div class="row">
                             <div class="col-6">
                                 <div class="mb-3">
                                     <label for="customer" class="form-label">Müşteri</label>
-                                    <select class="form-control" id="customer">
+                                    <select class="form-control" id="customer" name="customer">
                                         <option value="0">Seçin...</option>
                                         @if ($customers->count() > 0)
                                             @foreach ($customers as $customer)
@@ -45,7 +45,7 @@
                             <div class="col-6">
                                 <div class="mb-3">
                                     <label for="offer" class="form-label">Teklif</label>
-                                    <select class="form-control" id="offer">
+                                    <select class="form-control" id="offer" name="offer">
                                         <option value="0">Seçin...</option>
                                         @if ($offers)
                                             @foreach ($offers as $offer)
@@ -58,7 +58,7 @@
                         </div>
                         <div class="mb-3">
                             <label for="github" class="form-label">GitHub Repo</label>
-                            <input type="text" class="form-control" id="github" placeholder="Projenin görünen adını girin.">
+                            <input type="text" class="form-control" id="github" name="github" placeholder="Projenin görünen adını girin.">
                           </div>
                       </form>
                 </div>
@@ -71,7 +71,7 @@
                         <div class="mb-3">
                           <label for="price" class="form-label">Fiyat</label>
                           <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Projenin fiyatı">
+                            <input type="text" class="form-control" id="price" name="price" placeholder="Projenin fiyatı">
                             <span class="input-group-text">₺</span>
                             <span class="input-group-text">0.00</span>
                           </div>
@@ -81,7 +81,7 @@
 
                         <div class="mb-3">
                             <label for="start_date" class="form-label">Başlama Tarihi</label>
-                            <div class="input-group flatpickr" id="start_date">
+                            <div class="input-group flatpickr" id="start_date" name="start_date">
                                 <input type="text" class="form-control" placeholder="Select date" data-input>
                                 <span class="input-group-text input-group-addon" data-toggle><i data-feather="calendar"></i></span>
                             </div>
@@ -89,7 +89,7 @@
 
                           <div class="mb-3">
                             <label for="dead_line" class="form-label">Teslim Tarihi</label>
-                            <div class="input-group flatpickr" id="dead_line">
+                            <div class="input-group flatpickr" id="dead_line" name="dead_line">
                                 <input type="text" class="form-control" placeholder="Select date" data-input>
                                 <span class="input-group-text input-group-addon" data-toggle><i data-feather="calendar"></i></span>
                             </div>
@@ -97,7 +97,7 @@
 
                           <div class="mb-3">
                             <label class="form-label" for="techstack">Teknoloji Yığını</label>
-                            <select class="js-example-basic-multiple form-select" id="techstack" multiple="multiple" data-width="100%">
+                            <select class="js-example-basic-multiple form-select" id="techstack" name="techstack" multiple="multiple" data-width="100%">
                                 @if ($techs->count() > 0)
                                     @foreach ($techs as $tech)
                                         <option value="{{ $tech->id }}">{{$tech->title}}</option>

5c65db5d20212f016d0a26672b4b678af16b6e42
Alihan Öztürk <alihanozturk364@gmail.com>
2023-09-08 13:17:30 +0300

    sidebar icons changed1

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -14,14 +14,14 @@
         <li class="nav-item nav-category">#</li>
         <li class="nav-item">
           <a href="/" data-link="dashboard" class="nav-link">
-            <i class="link-icon" data-feather="box"></i>
+            <i class="link-icon" data-feather="compass"></i>
             <span class="link-title">Pano</span>
           </a>
         </li>
         <li class="nav-item nav-category">APP</li>
         <li class="nav-item">
           <a class="nav-link" data-bs-toggle="collapse" href="#projects" role="button" aria-expanded="false" aria-controls="projects">
-            <i class="link-icon" data-feather="mail"></i>
+            <i class="link-icon" data-feather="codepen"></i>
             <span class="link-title">Projeler</span>
             <i class="link-arrow" data-feather="chevron-down"></i>
           </a>
@@ -38,8 +38,29 @@
         </li>
 
         <li class="nav-item">
+          <a class="nav-link" data-bs-toggle="collapse" href="#offers" role="button" aria-expanded="false" aria-controls="offers">
+            <i class="link-icon" data-feather="file-text"></i>
+            <span class="link-title">Teklifer</span>
+            <i class="link-arrow" data-feather="chevron-down"></i>
+          </a>
+          <div class="collapse" id="offers">
+            <ul class="nav sub-menu">
+              <li class="nav-item">
+                <a href="/customer/new" class="nav-link">Yeni Teklif</a>
+              </li>
+              <li class="nav-item">
+                <a href="/customer/all" class="nav-link">Bekleyen Teklifler</a>
+              </li>
+              <li class="nav-item">
+                <a href="/customer/all" class="nav-link">Tüm Teklifler</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+
+        <li class="nav-item">
           <a class="nav-link" data-bs-toggle="collapse" href="#customers" role="button" aria-expanded="false" aria-controls="customers">
-            <i class="link-icon" data-feather="mail"></i>
+            <i class="link-icon" data-feather="users"></i>
             <span class="link-title">Müşteriler</span>
             <i class="link-arrow" data-feather="chevron-down"></i>
           </a>

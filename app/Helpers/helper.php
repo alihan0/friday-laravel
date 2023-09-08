0ea71168c3a8d164a6b0303396cf2c6be9c9acda
Alihan Öztürk <alihanozturk364@gmail.com>
2023-09-08 13:00:09 +0300

    helper function updated

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -3,21 +3,16 @@
 if (!function_exists('formatSeconds')) {
     function formatSeconds($seconds) {
         // Gün, saat ve dakikaları hesapla
-        $days = floor($seconds / (60 * 60 * 24));
-        $seconds -= $days * (60 * 60 * 24);
         $hours = floor($seconds / (60 * 60));
         $seconds -= $hours * (60 * 60);
         $minutes = floor($seconds / 60);
     
         // Zaman dilimlerini oluştur
         $result = '';
-        if ($days > 0) {
-            $result .= $days . ' Gün ';
-        }
         if ($hours > 0) {
             $result .= $hours . ' Saat ';
         }
-        if ($minutes > 0 && empty($result)) {
+        if ($minutes > 0 && $hours < 2) {
             $result .= $minutes . ' Dakika';
         }
     

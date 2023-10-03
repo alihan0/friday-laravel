b0a5f8c7e21d992f056b3c2344aa82b6455f63da
Alihan Öztürk <alihanozturk364@gmail.com>
2023-10-03 19:14:28 +0300

    version updated

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
     <p class="text-muted mb-1 mb-md-0">Copyright © 2023 <a href="https://www.nobleui.com" target="_blank">Metatige</a> | Tüm hakları saklıdır.</p>
-    <p class="text-muted">v{{config('app.version')}}</p>
+    <p class="text-muted">v1.1.95</p>
 </footer>
